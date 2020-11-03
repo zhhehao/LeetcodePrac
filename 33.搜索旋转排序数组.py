70913c18a957ecb40123969db14d3ea89cd86887
zhhehao <zh.hehao@gmail.com>
2020-11-03 10:35:53 +0800

    update p33

---
+++ 33.搜索旋转排序数组.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=33 lang=python3
+#
+# [33] 搜索旋转排序数组
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+
+# @lc code=end
+
