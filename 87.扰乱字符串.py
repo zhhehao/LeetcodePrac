ff7b4d001c858e93647dbb6dbac73999f673dc4f
zhhehao <zh.hehao@gmail.com>
2020-12-30 08:41:52 +0800

    87

---
+++ 87.扰乱字符串.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=87 lang=python3
+#
+# [87] 扰乱字符串
+#
+
+# @lc code=start
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        def sub_isScramble()
+# @lc code=end
+
